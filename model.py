197295694b8c6b995c42b24384c26fa6ef42abe4
ZeroHomer <56496833+ZeroHomer@users.noreply.github.com>
2023-03-23 14:55:03 +0800

    fix the bug

--- model.py
+++ model.py
@@ -179,70 +179,6 @@ class ConvLSTMVSR(nn.Module):
 
 
 class ConvGRUCell(nn.Module):
-    def __init__(self, input_size, input_dim, hidden_dim, kernel_size, bias, dtype):
-        """
-        Initialize the ConvLSTM cell
-        :param input_size: (int, int)
-            Height and width of input tensor as (height, width).
-        :param input_dim: int
-            Number of channels of input tensor.
-        :param hidden_dim: int
-            Number of channels of hidden state.
-        :param kernel_size: (int, int)
-            Size of the convolutional kernel.
-        :param bias: bool
-            Whether or not to add the bias.
-        :param dtype: torch.cuda.FloatTensor or torch.FloatTensor
-            Whether or not to use cuda.
-        """
-        super(ConvGRUCell, self).__init__()
-        self.height, self.width = input_size
-        self.padding = kernel_size[0] // 2, kernel_size[1] // 2
-        self.hidden_dim = hidden_dim
-        self.bias = bias
-        self.dtype = dtype
-
-        self.conv_gates = nn.Conv3d(in_channels=input_dim + hidden_dim,
-                                    out_channels=2 * self.hidden_dim,  # for update_gate,reset_gate respectively
-                                    kernel_size=kernel_size,
-                                    padding=self.padding,
-                                    bias=self.bias)
-
-        self.conv_can = nn.Conv3d(in_channels=input_dim + hidden_dim,
-                                  out_channels=self.hidden_dim,  # for candidate neural memory
-                                  kernel_size=kernel_size,
-                                  padding=self.padding,
-                                  bias=self.bias)
-
-    def init_hidden(self, batch_size):
-        return torch.zeros(batch_size, self.hidden_dim, self.height, self.width).type(self.dtype)
-
-    def forward(self, input_tensor, h_cur):
-        """
-        :param self:
-        :param input_tensor: (b, c, h, w)
-            input is actually the target_model
-        :param h_cur: (b, c_hidden, h, w)
-            current hidden and cell states respectively
-        :return: h_next,
-            next hidden state
-        """
-        combined = torch.cat([input_tensor, h_cur], dim=1)
-        combined_conv = self.conv_gates(combined)
-
-        gamma, beta = torch.split(combined_conv, self.hidden_dim, dim=1)
-        reset_gate = torch.sigmoid(gamma)
-        update_gate = torch.sigmoid(beta)
-
-        combined = torch.cat([input_tensor, reset_gate * h_cur], dim=1)
-        cc_cnm = self.conv_can(combined)
-        cnm = torch.tanh(cc_cnm)
-
-        h_next = (1 - update_gate) * h_cur + update_gate * cnm
-        return h_next
-
-
-class ConvGRUCell(nn.Module):
     def __init__(self, input_channels, hidden_channels, kernel_size):
         super(ConvGRUCell, self).__init__()
         self.input_channels = input_channels
@@ -279,20 +215,6 @@ class ConvGRUCell(nn.Module):
         h_t = update * h_t + (1 - update) * out
 
         return h_t
-
-    def init_weight(self):
-        # init conv using xavier
-        for m in self.modules():
-            if isinstance(m, nn.Conv3d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-
-
-class Decoder(nn.Module):
-    def __init__(self, input_channels, hidden_channels, kernel_size, padding, num_layers):
-        super(Decoder, self).__init__()
-        self.input_channels = input_channels
 
 
 class ConvGRU(nn.Module):
