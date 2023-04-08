c434cd34c9cad2da07eb42d62905e5ae0b9dd2da
ZeroHomer <56496833+ZeroHomer@users.noreply.github.com>
2023-04-08 10:31:00 +0800

    update

--- train.py
+++ train.py
@@ -106,7 +106,7 @@ if __name__ == '__main__':
     transform = transforms.Compose({
         RandomFlip3D()
     })
-    seq_num = 6
+    seq_num = 8
     train_set = VolumesDataset(opt.train_data_dir, dim, dsam_scale_factor, transform, seq_num=seq_num)
     val_set = VolumesDataset(opt.val_data_dir, dim, dsam_scale_factor, seq_num=0)
     train_loader = DataLoader(train_set, batch_size=opt.train_batch_size, shuffle=True)
